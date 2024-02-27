c36b43adcf4cb79d14ddfb1ba401faba4bd2bf0a
gunhee98 <rjsgmlaha1@gmail.com>
2024-02-28 04:49:02 +0900

    패러렐 라우트 및 인터셉팅 라우트 셋팅 홈화면 레이아웃 구성

---
+++ src/app/(beforeLogin)/@modal/(.)i/flow/login/page.tsx
@@ -0,0 +1,7 @@
+import LoginModal from "@/app/(beforeLogin)/_component/LoginModal";
+import style from "../login/login.module.css";
+import { useState } from "react";
+
+export default function Page() {
+  return <LoginModal />;
+}
--- src/app/(beforeLogin)/i/flow/login/page.tsx
+++ src/app/(beforeLogin)/i/flow/login/page.tsx
@@ -1,7 +1,7 @@
+import LoginModal from "@/app/(beforeLogin)/_component/LoginModal";
+import style from "../login/login.module.css";
+import { useState } from "react";
+
 export default function Page() {
-  return (
-    <main>
-      로그인모달
-    </main>
-  );
+  return <LoginModal />;
 }
